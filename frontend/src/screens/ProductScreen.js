acfd31dab1fa3bb7e3f7f0c3711c20f3a394047f
kakiche6 <arezki.inventeam@gmail.com>
2020-11-27 21:17:04 +0100

    add to cart functionality

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import Loader from '../components/Loader.js';
 import Message from '../components/Message.js';
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
 
   const dispatch = useDispatch();
   const { loading, error, product } = useSelector(
