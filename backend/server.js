efdf6b8049b17193992033e7654492abb092e377
kakiche6 <arezki.inventeam@gmail.com>
2020-12-03 20:16:16 +0100

    1)shipping screen & save address - 2)Checkout Steps Component - 3. Payment Screen & Save Payment Method  4. Place Order Screen - 5. Order Controller & Route - 6. Create Order

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
