0e851bd2060d233c9e9fbd1993576be51fceebb4
kakiche6 <arezki.inventeam@gmail.com>
2020-12-04 09:57:02 +0100

    order screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
           <Route path='/login' component={LoginScreen} exact />
           <Route path='/placeorder' component={PlaceOrderScreen} exact />
           <Route path='/payment' component={PaymentScreen} exact />
+          <Route path='/order/:id' component={OrderScreen} exact />
           <Route path='/shipping' component={ShippingScreen} exact />
           <Route path='/register' component={RegisterScreen} exact />
           <Route path='/profile' component={ProfileScreen} exact />
