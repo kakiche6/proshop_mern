7be8bd04850a02f668811ebb5ac4b7c4a709cf2c
kakiche6 <arezki.inventeam@gmail.com>
2020-12-05 12:21:35 +0100

    front end product reviews

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -22,6 +22,9 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
+              <LinkContainer to='/'>
+                <Nav.Link>Store</Nav.Link>
+              </LinkContainer>
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <i className='fas fa-shopping-cart'></i> Cart
